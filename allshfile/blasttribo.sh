60884162ad185cb8deb5497ec621cf22a03abe4d
Xuyue_Yang <yangxuyue0222@gmail.com>
2021-11-12 23:07:44 +0100

    Update blasttribo.sh

--- allshfile/blasttribo.sh
+++ allshfile/blasttribo.sh
@@ -19,6 +19,6 @@ cd /proj/b2013028/INBOX/private/tribolium
 #/sw/apps/bioinfo/blast/2.2.31+/milou/bin/makeblastdb -in uniprottriboliumnonmt.fasta -dbtype prot
 
 /sw/apps/bioinfo/blast/2.2.31+/milou/bin//blastx -query /proj/b2013028/INBOX/private/trinityfilter/longestisocalm.fasta -db uniprottriboliumnonmt.fasta -out blastxtriboliso.outfmt6 \
- -evalue 1e-20 -num_threads 6 -max_target_seqs 1 -outfmt 6
+ -evalue 1e-20 -num_threads 6 -outfmt 6
  
 /sw/apps/bioinfo/trinity/2.1.0/milou/util/analyze_blastPlus_topHit_coverage.pl /proj/b2013028/INBOX/private/tribolium/blastxtriboliso.outfmt6 /proj/b2013028/INBOX/private/trinityfilter/longestisocalm.fasta /proj/b2013028/INBOX/private/tribolium/uniprottriboliumnonmt.fasta
